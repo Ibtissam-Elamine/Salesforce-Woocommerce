26a6ee75e1f8616b18902cc55c8fd9ba9c75e5cc
chaymaeben4 <chaymaebenlagraoui47@gmail.com>
2024-09-13 13:40:34 +0100

    commit

--- force-app/main/default/lwc/searchInterface/searchInterface.js
+++ force-app/main/default/lwc/searchInterface/searchInterface.js
@@ -1,13 +1,9 @@
 import { LightningElement, track , wire} from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import searchWooCommerceData from '@salesforce/apex/WC_SearchController.searchWooCommerceData';
-import updateInSalesforce from '@salesforce/apex/UpsertInSalesforce.updateInSalesforce';
-import { getPicklistValues } from 'lightning/uiObjectInfoApi';
-import CATEGORY_FIELD from '@salesforce/schema/wc_Product__c.wc_category__c';
-import STATUS_FIELD from '@salesforce/schema/wc_Product__c.wc_status__c';
-import PURCHASABLE_FIELD from '@salesforce/schema/wc_Product__c.wc_purchaseable__c';
-import picklistCell from 'c/picklistCell';
-
+import synchronizeCustomer from '@salesforce/apex/SynchCustomer.synchronizeCustomer';
+import SynchProduct from '@salesforce/apex/SynchProduct.SynchProduct';
+import SynchronizeOrder from '@salesforce/apex/SynchOrder.SynchronizeOrder';
 export default class SearchInterface extends LightningElement {
     @track searchKey = '';
     @track selectedObject = '';
@@ -17,17 +13,7 @@ export default class SearchInterface extends LightningElement {
     @track isEditable = false;
     @track isInvalid = true;
     @track errors = {};
-    
 
-
-    @track categoryOptions = [];
-    statusOptions = [
-        { label: 'Publish', value: 'publish' },
-        { label: 'Draft', value: 'draft' },
-        { label: 'Pending', value: 'pending' },
-        { label: 'Private', value: 'private' }
-    ];
-        @track purchasableOptions = [];
 
     
     objectOptions = [
@@ -37,37 +23,7 @@ export default class SearchInterface extends LightningElement {
         { label: 'Inventory', value: 'wc_inventory__c' },
         { label: 'Product', value: 'wc_product__c' }
     ];
-    get inputClass() {
-        // return this.isInvalid ? 'input-error' : 'input-valid';
-    }
-  
-    // Wire methods to fetch picklist values for the wc_product__c object fields
-    @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: CATEGORY_FIELD })
-    wiredCategoryValues({ error, data }) {
-        if (data) {
-            this.categoryOptions = data.values;
-        } else if (error) {
-            console.error('Error fetching category picklist values:', error);
-        }
-    }
-
-    // @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: STATUS_FIELD  })
-    // wiredStatusValues({ error, data }) {
-    //     if (data) {
-    //         this.statusOptions = data.values;
-    //     } else if (error) {
-    //         console.error('Error fetching status picklist values:', error);
-    //     }
-    // }
-
-    @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: PURCHASABLE_FIELD })
-    wiredPurchasableValues({ error, data }) {
-        if (data) {
-            this.purchasableOptions = data.values;
-        } else if (error) {
-            console.error('Error fetching purchasable picklist values:', error);
-        }
-    }
+   
 
     handleObjectChange(event) {
         this.selectedObject = event.detail.value;
@@ -83,32 +39,6 @@ export default class SearchInterface extends LightningElement {
         // this.isInvalid = this.searchKey.length < 3;
     }
 
-    handleCellChange(event) {
-        const draftValues = event.detail.draftValues;
-        this.errors = {}; // Clear any previous errors
-
-        draftValues.forEach((draft) => {
-            // Check if the changed field is 'email'
-            if (draft.email && !this.validateEmail(draft.email)) {
-                this.errors = {
-                    rows: {
-                        [draft.id]: {
-                            title: 'Invalid email format',
-                            fieldNames: ['email']
-                        }
-                    }
-                };
-            }
-        });
-        // this.draftValues = [...this.draftValues, ...draftValues];
-
-    }
-
-    // Method to validate email format
-    validateEmail(email) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    }
 
 
    handleSearch() {
@@ -122,114 +52,58 @@ export default class SearchInterface extends LightningElement {
     }
 
    
-    handleSave(event) {
-        this.draftValues = event.detail.draftValues;
-    
-        // Check for any existing errors before saving
-        if (Object.keys(this.errors).length > 0) {
-            console.log('Errors detected:', this.errors);
-    
-            // Show an error message
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Error updating records',
-                    message: 'Please fix the errors before saving.',
-                    variant: 'error',
-                })
-            );
-            return; // Exit the function if there are errors
-        }
-    
-        console.log('Search Results:', JSON.stringify(this.searchResult, null, 2));
-        console.log('Draft Values:', JSON.stringify(this.draftValues, null, 2));
-    
-        let updatedRecords = [];
-    
-        this.draftValues.forEach(draftValue => {
-            console.log('Processing Draft Value:', JSON.stringify(draftValue, null, 2));
-    
-            let recordToUpdate = this.searchResult.find(record => String(record.id) === String(draftValue.id));
-    
-            if (recordToUpdate) {
-                let updatedRecord = { ...recordToUpdate };
-    
-                for (const field in draftValue) {
-                    if (draftValue.hasOwnProperty(field)) {
-                        updatedRecord[field] = draftValue[field];
-                    }
-                }
-    
-                updatedRecords.push(updatedRecord);
-    
-                console.log('Updated Record to be Upserted:', JSON.stringify(updatedRecord, null, 2));
-            } else {
-                console.warn('No matching record found for draft value ID:', draftValue.id);
-            }
-        });
-    
-        console.log('Updated Records:', JSON.stringify(updatedRecords, null, 2));
-    
-        updateInSalesforce({ data: updatedRecords, objectType: this.selectedObject })
-            .then(result => {
-                console.log('Upsert successful:', result);
-    
-                // Show a success message
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Success',
-                        message: 'Records have been updated successfully.',
-                        variant: 'success',
-                    })
-                );
-                this.draftValues = [];
-                this.errors = {};
-                this.handleSearch(); // Refresh the table data
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                console.log('Error Body:', error.body);
-    
-                // Show an error message
-                this.dispatchEvent(
-                    new ShowToastEvent({
-                        title: 'Error updating records',
-                        message: error.body ? error.body.message : 'An unknown error occurred',
-                        variant: 'error',
-                    })
-                );
-            });
-    }
     
     
-    
-    
-
 
     getColumnsForObject(objectName) {
         switch (objectName) {
             case 'wc_customer__c':
                 return [
                     { label: 'ID', fieldName: 'id' },
-                    { label: 'First Name', fieldName: 'first_name', editable: true },
-                    { label: 'Last Name', fieldName: 'last_name', editable: true },
-                    { label: 'Email', fieldName: 'email', editable: true },
-                    { label: 'Phone', fieldName: 'phone', editable: true },
-                    { label: 'Date Created', fieldName: 'date_created', editable : true , type: 'date' },
+                    { label: 'First Name', fieldName: 'first_name'  },
+                    { label: 'Last Name', fieldName: 'last_name'  },
+                    { label: 'Email', fieldName: 'email'  },
+                    { label: 'Phone', fieldName: 'phone'  },
+                    { label: 'Date Created', fieldName: 'date_created', type: 'date' },
                     { label: 'Billing Address', fieldName: 'billing_address' },
                     { label: 'Shipping Address', fieldName: 'shipping_address' },
-                    { label: 'Last Order Date' , fieldName: 'lastOrderDate' , editable: true , type : 'date'},
-                    { label: 'Total Orders' , fieldName: 'totalOrders' , editable : true}
+                    { label: 'Last Order Date' , fieldName: 'lastOrderDate' ,  type : 'date'},
+                    { label: 'Total Orders' , fieldName: 'totalOrders' },
+                    { label: 'Last Sync Date', fieldName: 'LastSyncDate'},
+                    {
+                        label: 'Actions',
+                        type: 'button',
+                        typeAttributes: {
+                            label: 'Synchronize',
+                            name: 'synchronize',
+                            title: 'Click to synchronize with Salesforce',
+                            disabled: { fieldName: 'isExistingInSalesforce' }, // Désactivé si non existant
+                            variant: 'brand'
+                        }
+                    }
                 ];
             case 'wc_order__c':
                 return [
                     { label: 'ID', fieldName: 'id' },
                     { label: 'Customer', fieldName: 'customer_id' },
                     { label: 'Order Number', fieldName: 'order_number' },
-                    { label: 'Status', fieldName: 'status' , editable:true},
-                    { label: 'Currency', fieldName: 'currency' , editable :true},
-                    { label: 'Total', fieldName: 'total',  editable : true},
-                    { label: 'Payment Method', fieldName: 'payment_method' , editable : true },
-                    { label: 'Date Paid', fieldName: 'date_paid' , editable : true , type : 'date' }
+                    { label: 'Status', fieldName: 'status'},
+                    { label: 'Currency', fieldName: 'currency' },
+                    { label: 'Total', fieldName: 'total'},
+                    { label: 'Payment Method', fieldName: 'payment_method'  },
+                    { label: 'Date Paid', fieldName: 'date_paid' ,  type : 'date' },
+                    { label: 'Last Sync Date', fieldName: 'LastSyncDate' },
+                    {
+                        label: 'Actions',
+                        type: 'button',
+                        typeAttributes: {
+                            label: 'Synchronize',
+                            name: 'synchronize',
+                            title: 'Click to synchronize with Salesforce',
+                            disabled: { fieldName: 'isExistingInSalesforce' }, // Désactivé si non existant
+                            variant: 'brand'
+                        }
+                    }
                 ];
             // case 'wc_OrderItem__c':
             //     return [
@@ -244,48 +118,118 @@ export default class SearchInterface extends LightningElement {
                 return [
                     { label: 'Id', fieldName: 'id' },
                     { label: 'Product ID', fieldName: 'product_id'  },
-                    { label: 'Available Quantity', fieldName: 'quantity' , editable : true },
+                    { label: 'Available Quantity', fieldName: 'quantity'  },
                     { label: 'Last Stock Update', fieldName: 'last_Stock_Update' },
-                    { label: 'Status', fieldName: 'status' , editable : true },
-                    { label: 'Weight', fieldName: 'weight' , editable : true }
+                    { label: 'Status', fieldName: 'status' },
+                    { label: 'Weight', fieldName: 'weight'  },
+                    
                 ];
             case 'wc_product__c':
                 return [
                     { label: 'Id', fieldName: 'id' },
-                    { label: 'Product Name', fieldName: 'name', editable : true },
-                    { label: 'Category', fieldName: 'category', editable: true, type: 'picklistCell',
-                        typeAttributes: {
-                            placeholder: 'Choose Category',
-                            options: this.categoryOptions, 
-                            value: { fieldName: 'category' },
-                            context: { fieldName: 'id' }
-                        }
-                    },
-                    { label: 'Price', fieldName: 'price' , editable : true },
-                    { label: 'Stock Quantity', fieldName: 'stock_quantity' , editable : true },
-                    { 
-                        label: 'Status', 
-                        fieldName: 'status', 
-                        type: 'picklistCell',
-                        typeAttributes: {
-                            placeholder: 'Choose Status',
-                            options: this.statusOptions,
-                            value: { fieldName: 'status' },
-                            context: { fieldName: 'id' }
-                        },
-                        editable: true 
-                    },
+                    { label: 'Product Name', fieldName: 'name'},
+                    { label: 'Category', fieldName: 'category'},
+                    { label: 'Price', fieldName: 'price'},
+                    { label: 'Stock Quantity', fieldName: 'stock_quantity'},
+                    { label: 'Status', fieldName: 'status'},
                     { label: 'Total Sales', fieldName: 'total_sales' },
-                    { label: 'Purchasable', fieldName: 'purchasable', editable: true, type: picklistCell,
+                    { label: 'Purchasable', fieldName: 'purchasable'},
+                    { label: 'Last Sync Date', fieldName: 'LastSyncDate'},
+                    {
+                        label: 'Actions',
+                        type: 'button',
                         typeAttributes: {
-                            placeholder: 'Choose Purchasable',
-                            options: this.purchasableOptions.map(opt => ({ label: opt.label, value: opt.value })),
-                            value: { fieldName: 'purchasable' },
-                            context: { fieldName: 'id' }
+                            label: 'Synchronize',
+                            name: 'synchronize',
+                            title: 'Click to synchronize with Salesforce',
+                            disabled: { fieldName: 'isExistingInSalesforce' }, // Désactivé si non existant
+                            variant: 'brand'
                         }
-                    },
+                    }
                     ];
             default:
                 return [];
         }
-    }}
+    }
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+        console.log('selected object ' , this.selectedObject);
+        if (actionName === 'synchronize') {
+            if(this.selectedObject==='wc_customer__c'){
+                this.synchronizeCustomer(row.id);
+            }
+            if(this.selectedObject==='wc_product__c'){
+                this.synchronizeProduct(row.id);
+            }  
+            if(this.selectedObject==='wc_order__c') {
+                this.synchronizeOrder(row.id);
+            } 
+        }
+        console.log('row id ' , row.id);
+    }
+
+    synchronizeCustomer(recordId) {
+        synchronizeCustomer({ recordId })
+            .then(result => {
+                this.showToast('Success', result, 'success');
+               // Rafraîchir les données après synchronisation
+            })
+            .catch(error => {
+                this.showToast('Error', error.body.message, 'error');
+                this.errors = error;
+            });
+    }
+    
+    synchronizeProduct(recordId) {
+        SynchProduct({ recordId })
+            .then(result => {
+                this.showToast('Success', result, 'success');
+                this.handleSearch(); // Rafraîchir les données après synchronisation
+            })
+            .catch(error => {
+                this.showToast('Error', error.body.message, 'error');
+                this.errors = error;
+            });
+    }
+    synchronizeOrder(recordId) {
+        SynchronizeOrder({ recordId })
+            .then(result => {
+                this.showToast('Success', result, 'success');
+                this.handleSearch(); // Rafraîchir les données après synchronisation
+            })
+            .catch(error => {
+                this.showToast('Error', error.body.message, 'error');
+                this.errors = error;
+            });
+    }
+
+
+
+
+
+
+    showToast(title, message, variant) {
+        const event = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant
+        });
+        this.dispatchEvent(event);
+    }
+
+    refreshRow(recordId) {
+        searchWooCommerceData({ objectType: this.selectedObject, key: recordId })
+            .then(result => {
+                // Mettre à jour la ligne correspondante dans searchResult
+                const index = this.searchResult.findIndex(item => item.id === recordId);
+                if (index !== -1) {
+                    this.searchResult[index] = JSON.parse(JSON.stringify(result[0]));
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
+    
+}
